479f733185c812b9c549e3bc74055ae936dd21a2
cegarc5579 <carlos_garcia1@baylor.edu>
2022-05-01 19:48:52 -0500

    finished video 1

--- FeedApp/views.py
+++ FeedApp/views.py
@@ -18,8 +18,42 @@ def index(request):
 
 
 
-#@login_required
+@login_required
+def profile(request):
+    #checking to see if they have a profile first, if not we create one for them
+    profile = Profile.objects.filter(user=request.user) #request.user refers to the one who is logged on and using the system
+    if not profile.exists():
+        #checking to see if the first line brought back any data
+        #if not then we will create it for them
+        Profile.objects.create(user=request.user)#this creates a file for them
+    profile = Profile.objects.get(user=request.user)
 
+    if request.method != 'POST':
+        form = ProfileForm(instance=profile)
+    else:
+        form = ProfileForm(instance=profile,data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('FeedApp:profile')#this keeps the user on the profile page 
+    
+    context = {'form': form}
+    return render(request, 'FeedApp/profile.html', context)
+
+@login_required
+def myfeed(request):
+    comment_count_list = []
+    like_count_list = []
+    posts = Post.objects.filter(username=request.user).order_by('-date_posted')#ordering them by the newest post showing at the top by using orderby
+    for p in posts:
+        c_count = Comment.objects.filter(post=p).count()
+        l_count = Like.objects.filter(post=p).count()
+        #these above count likes and comments
+        comment_count_list.append(c_count)
+        like_count_list.append(l_count)
+    zipped_list = zip(posts,comment_count_list,like_count_list)
+
+    context = {'posts':posts, 'zipped_list':zipped_list}
+    return render(request, 'FeedApp/myfeed.html', context)
 
 
 
